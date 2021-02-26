53900a372da53ea8c5d0d1a07bc968c9b7aaf2a2
Arthur Pedroti <arthurpedroti@gmail.com>
2021-02-26 14:49:32 -0300

    fix: test

--- src/controllers/OPsController.js
+++ src/controllers/OPsController.js
@@ -55,31 +55,7 @@ module.exports = {
             `, function (err, recordset) {
             
             if (err) console.log(err)
-              console.log(
-                `
-                SELECT  RTRIM(SC2.C2_PRODUTO) AS PRODUTO,
-                        RTRIM(SB1.B1_DESC) AS DESCRICAO,
-                        SC2.C2_NUM + SC2.C2_ITEM + SC2.C2_SEQUEN AS OP,
-                        RTRIM(SC2.C2_CC) AS CC,
-                        SC2.C2_QUANT AS QTD,
-                        CONCAT(SUBSTRING(SC2.C2_DATPRI,7,2),'/',SUBSTRING(SC2.C2_DATPRI,5,2),'/',SUBSTRING(SC2.C2_DATPRI,1,4)) AS DAT_INI,
-                        CONCAT(SUBSTRING(SC2.C2_DATPRF,7,2),'/',SUBSTRING(SC2.C2_DATPRF,5,2),'/',SUBSTRING(SC2.C2_DATPRF,1,4)) AS DAT_FIM,
-                        RTRIM(SC2.C2_OBS) AS OBS,
-                        CONCAT(SUBSTRING(SC2.C2_EMISSAO,7,2),'/',SUBSTRING(SC2.C2_EMISSAO,5,2),'/',SUBSTRING(SC2.C2_EMISSAO,1,4)) AS DAT_EMI,
-                        SC2.C2_QUJE AS QTD_PRO
-    
-                FROM    SC2010 AS SC2 LEFT OUTER JOIN
-                        SB1010 AS SB1 ON SB1.D_E_L_E_T_ = '' AND SB1.B1_COD = SC2.C2_PRODUTO
-    
-                WHERE   ${filial_condition}
-                        ${produto_condition}
-                        ${obs_condition}
-                        ${opnumber_condition}
-                        (SC2.C2_DATRF = '') AND 
-                        (SC2.D_E_L_E_T_ = '')
-    
-                ORDER BY SC2.C2_DATPRI
-                `)
+             
             return res.json(recordset.recordsets[0]);
             // send records as a response
             }
