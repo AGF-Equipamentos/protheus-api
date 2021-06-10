a4f5ad2068f779b76f5541961192a927325c8a9a
Arthur Pedroti <arthurpedroti@gmail.com>
2021-06-10 09:12:51 -0300

    feat: allow croos orgin3

--- src/server.js
+++ src/server.js
@@ -8,20 +8,8 @@ require('./database');
 
 const app = express();
 
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 app.use(cors());
 app.use(express.json());
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 app.use(routes);
 
 app.listen(process.env.PORT);
