87d4df10f738b56d852ab3cf04b97bbd523e15c9
Arthur Pedroti <arthurpedroti@gmail.com>
2022-03-31 15:44:48 -0300

    fix: ajust ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  apps : [{
-    name: 'protheus-api',
-    script: 'src/server.js',
-    interpreter: 'node@16.14.1'
-  }]
-};
+  apps: [
+    {
+      name: 'protheus-api',
+      script: 'src/server.js',
+      interpreter: 'node@16.14.1'
+    }
+  ]
+}
