40080b60794f6f8e69527fd98483584a8012cb54
Arthur Pedroti <arthurpedroti@gmail.com>
2022-09-21 16:53:33 -0300

    fix: kardex data_inicio default kardex

--- src/controllers/KardexController.js
+++ src/controllers/KardexController.js
@@ -9,7 +9,7 @@ module.exports = {
     const {
       filial = '0101',
       produto = '',
-      data_inicio = new Date(2000, 0, 1),
+      data_inicio = new Date(2022, 0, 1),
       data_fim = new Date(2099, 0, 1),
       armazem_inicio = '01',
       armazem_fim = '01'
@@ -17,34 +17,34 @@ module.exports = {
 
     await request.query(
       `
-      SELECT * 
+      SELECT *
       FROM (
-  
-          SELECT * 
-          FROM vw_stock_balance WITH (NOLOCK) 
-          WHERE [Data Mov.] = ( 
-                SELECT TOP 1 B9_DATA 
+
+          SELECT *
+          FROM vw_stock_balance WITH (NOLOCK)
+          WHERE [Data Mov.] = (
+                SELECT TOP 1 B9_DATA
               FROM SB9010 SB9 WITH (NOLOCK)
-              WHERE SB9.D_E_L_E_T_ = '' 
+              WHERE SB9.D_E_L_E_T_ = ''
                     AND B9_FILIAL = '${filial}'
                     AND B9_DATA < '${format(new Date(data_inicio), 'yyyyMMdd')}'
               ORDER BY B9_DATA DESC
-          ) 
-              
+          )
+
           UNION ALL
-          
+
           SELECT *
-          FROM vw_stock_movements WITH (NOLOCK) 
+          FROM vw_stock_movements WITH (NOLOCK)
           WHERE [Data Mov.] BETWEEN '${format(
             new Date(data_inicio),
             'yyyyMMdd'
           )}' AND '${format(new Date(data_fim), 'yyyyMMdd')}'
-          
-      ) AS TMP 
-      
+
+      ) AS TMP
+
       WHERE [Filial] = '${filial}'
             AND [Produto] = '${produto}'
-            AND [Armazem] BETWEEN '${armazem_inicio}' AND '${armazem_fim}'      
+            AND [Armazem] BETWEEN '${armazem_inicio}' AND '${armazem_fim}'
       ORDER BY [Data Mov.],
                [Sequencia],
                [CFOP] DESC
