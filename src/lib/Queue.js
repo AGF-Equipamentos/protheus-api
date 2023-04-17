107d290e7b17caffea519f7eea415379b488c0db
Arthur Pedroti <arthurpedroti@gmail.com>
2023-04-17 11:08:09 -0300

    fix: connection redis object on bull queue

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -4,7 +4,7 @@ const { redisConfig } = require('../database/redisconfig')
 const jobs = require('../jobs')
 
 const queues = Object.values(jobs).map((job) => ({
-  bull: new Queue(job.key, redisConfig),
+  bull: new Queue(job.key, { redis: redisConfig }),
   name: job.key,
   handle: job.handle,
   options: job.options
