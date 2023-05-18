c4c45d6ef71af40464877e80ab65428295061f3d
Arthur Pedroti <arthurpedroti@gmail.com>
2023-05-18 16:26:16 -0300

    undo: coment redis connections and functions

--- src/server.js
+++ src/server.js
@@ -1,26 +1,25 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { createBullBoard } = require('@bull-board/api')
-
 const routes = require('./routes')
-const Queue = require('./lib/Queue')
-const { ExpressAdapter } = require('@bull-board/express')
-const { BullAdapter } = require('@bull-board/api/bullAdapter')
+
+// const { createBullBoard } = require('@bull-board/api')
+// const Queue = require('./lib/Queue')
+// const { ExpressAdapter } = require('@bull-board/express')
+// const { BullAdapter } = require('@bull-board/api/bullAdapter')
 
 require('./database')
 
-const serverAdapter = new ExpressAdapter()
-serverAdapter.setBasePath('/admin/queues')
-
-createBullBoard({
-  queues: Queue.queues.map((queue) => new BullAdapter(queue.bull)),
-  serverAdapter: serverAdapter
-})
-
 const app = express()
 
-app.use('/admin/queues', serverAdapter.getRouter())
+// Bull Dashboard
+// const serverAdapter = new ExpressAdapter()
+// serverAdapter.setBasePath('/admin/queues')
+// createBullBoard({
+//   queues: Queue.queues.map((queue) => new BullAdapter(queue.bull)),
+//   serverAdapter: serverAdapter
+// })
+// app.use('/admin/queues', serverAdapter.getRouter())
 
 app.use(cors())
 app.use(express.json())
