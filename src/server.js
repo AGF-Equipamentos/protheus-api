bcdb4207c0b318fdfb113f54a1a61f6c57b7ad81
Arthur Pedroti <arthurpedroti@gmail.com>
2024-01-30 15:12:37 -0300

    feat: add sentry to log errors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
+const Sentry = require('@sentry/node')
 
 const { createBullBoard } = require('@bull-board/api')
 const Queue = require('./lib/Queue')
@@ -11,6 +12,14 @@ const { BullAdapter } = require('@bull-board/api/bullAdapter')
 require('./database')
 
 const app = express()
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0
+})
+
+app.use(Sentry.Handlers.requestHandler())
+app.use(Sentry.Handlers.tracingHandler())
 
 // Bull Dashboard
 const serverAdapter = new ExpressAdapter()
@@ -24,5 +33,7 @@ app.use('/admin/queues', serverAdapter.getRouter())
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+
+app.use(Sentry.Handlers.errorHandler())
 
 app.listen(process.env.PORT)
