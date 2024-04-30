3c69f4b8c8e28e4382bdb79debe665ba1a909115
ronaldoTIrodrigues <rrodriguesdelimafilho@gmail.com>
2024-04-30 14:02:37 -0300

    featUpdateQueryStockControllers

--- src/controllers/EstoqueController.js
+++ src/controllers/EstoqueController.js
@@ -4,7 +4,7 @@ module.exports = {
   async index(req, res) {
     const request = new sql.Request()
 
-    //querry request need to declare all the variables!
+    //query request need to declare all the variables!
     const { filial, produto, grupo, armazem } = req.query
 
     let filial_condition
@@ -51,17 +51,71 @@ module.exports = {
     // query to the database and get the records
     await request.query(
       `
-            SELECT FILIAL,PRODUTO,DESCRICAO,SALDO,ARMAZEM
-            FROM SALDO_ESTOQUE WITH (NOLOCK)
-            WHERE
-            ${produto_condition}
-            ${grupo_condition}
-            ${filial_condition}
-            ${armazem_condition}
-            SALDO>0
-            ORDER BY 'PRODUTO'
+      SELECT *
+      FROM (
+        SELECT FILIAL, PRODUTO, DESCRICAO, SALDO, ARMAZEM, GRUPO
+        FROM SALDO_ESTOQUE WITH (NOLOCK)
+        WHERE
+        SALDO > 0
+                  
+        UNION ALL
+                  
+        SELECT SB6.B6_FILIAL AS FILIAL, SB6.B6_PRODUTO AS PRODUTO, SB1.B1_DESC AS DESCRICAO, SB6.B6_SALDO AS SALDO, 'FEIRA' AS ARMAZEM, SB1.B1_GRUPO AS GRUPO
+        FROM dbo.SB6010 AS SB6 WITH (NOLOCK)
+        LEFT OUTER JOIN dbo.SB1010 AS SB1 ON LEFT(SB6.B6_FILIAL, 2) = LEFT('0101,0102,0103,0104,0105,0106', 2) AND SB6.B6_PRODUTO = SB1.B1_COD
+        WHERE
+        SB6.B6_TES IN ('554', '680') AND
+        SB6.B6_ATEND <> 'S' AND
+        SB6.B6_SALDO > 0 AND
+        SB6.D_E_L_E_T_ = ''
+        GROUP BY B6_FILIAL, B6_PRODUTO, B1_DESC, B6_SALDO, B1_GRUPO
+                  
+      ) AS QUERY
+
+      WHERE
+          ${produto_condition}
+          ${grupo_condition}
+          ${filial_condition}
+          ${armazem_condition}
+              SALDO > 0 
             `,
+
       function (err, recordset) {
+        console.log(`
+        SELECT *
+        FROM (
+          SELECT FILIAL, PRODUTO, DESCRICAO, SALDO, ARMAZEM, GRUPO
+          FROM SALDO_ESTOQUE WITH (NOLOCK)
+          WHERE
+          SALDO > 0
+                    
+          UNION ALL
+                    
+          SELECT
+          SB6.B6_FILIAL AS FILIAL,
+          SB6.B6_PRODUTO AS PRODUTO,
+          SB1.B1_DESC AS DESCRICAO,
+          SUM(SB6.B6_SALDO) AS SALDO,
+          'FEIRA' AS ARMAZEM,
+          SB1.B1_GRUPO AS GRUPO
+          FROM dbo.SB6010 AS SB6 WITH (NOLOCK)
+          LEFT OUTER JOIN dbo.SB1010 AS SB1 ON LEFT(SB6.B6_FILIAL, 2) = LEFT('0101,0102,0103,0104,0105,0106', 2) AND SB6.B6_PRODUTO = SB1.B1_COD
+          WHERE
+          SB6.B6_TES IN ('554', '680') AND
+          SB6.B6_ATEND <> 'S' AND
+          SB6.B6_SALDO > 0 AND
+		      SB6.D_E_L_E_T_ = ''
+		      GROUP BY B6_FILIAL, B6_PRODUTO, B1_DESC, B6_SALDO, B1_GRUPO
+                    
+        ) AS QUERY
+  
+        WHERE
+              ${produto_condition}
+              ${grupo_condition}
+              ${filial_condition}
+              ${armazem_condition}
+                SALDO > 0
+              `)
         if (err) {
           console.log(err)
           return res.json({
